b8473a8009c30a7bf68ce3f05919e6bf9a33a088
User <User@DESKTOP-R7OCC2A>
2023-12-26 17:07:25 +0900

    chap02 Commit

---
+++ chap02/src/main/java/chap02/exam05/Start5.java
@@ -0,0 +1,22 @@
+package chap02.exam05;
+
+public class Start5 {
+
+	static int share;
+	
+	public static void main(String[] args) {
+		share = 55;
+		
+		int k = fun(5, 7);
+		
+		System.out.println(share);
+
+	}
+		
+	private static int fun(int m, int p) {
+		share = m + p;
+		
+		return m - p;
+	}
+
+}
